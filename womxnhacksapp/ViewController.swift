67706a5386f9f2ae14e58aa36b2537a3da595669
Anisha Kabir <ahsina.ribak@gmail.com>
2020-11-15 15:09:11 -0800

    finished layout for home screen

--- womxnhacksapp/ViewController.swift
+++ womxnhacksapp/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var EventsButton: UIButton!
+    @IBOutlet weak var CalendarButton: UIButton!
+    @IBOutlet weak var AnnouncementsButton: UIButton!
+    @IBOutlet weak var SponsorsButton: UIButton!
+    @IBOutlet weak var ProfileButton: UIButton!
+    @IBOutlet weak var MapButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
-    }
+        EventsButton.layer.cornerRadius = 10
+        CalendarButton.layer.cornerRadius = 10
+        AnnouncementsButton.layer.cornerRadius = 10
+        SponsorsButton.layer.cornerRadius = 10
+        ProfileButton.layer.cornerRadius = 10
+        MapButton.layer.cornerRadius = 10
 
+    }
+    
 
 }
 
