2f67eb66acca6126399c3926eb143475b6122a6b
Anisha Kabir <ahsina.ribak@gmail.com>
2021-02-28 16:32:45 -0800

    added workshop titles and times to events page

--- womxnhacksapp/EventsViewController.swift
+++ womxnhacksapp/EventsViewController.swift
@@ -11,16 +11,21 @@ class EventsViewController: UIViewController, UITableViewDataSource, UITableView
     
     @IBOutlet weak var eventsTableView: UITableView!
     
+    let eventTitles = ["Opening Ceremony", "Dinner and Team Formation", "Intro to iOS Workshop", "Queer and Trans 101 Workshop", "Scavenger Hunt", "Morning Meditation", "Intro to Unity", "Intro to Hardware", "Lunch", "Company Expo", "Navigating a Tech Career as a Woman Panel", "Coffee Conversations", "Intro to Deep Learning", "How to be an Ally Workshop", "Buzzfeed Quizzes", "How to Land a Job/Interview and Intro to Whatever", "Movie Night", "Devpost Submissions", "Demos for Judging", "Closing Ceremony and Prizes"]
+    let times = ["Friday 4/9, 5:00 - 6:30 pm", "Friday 4/9, 6:30 - 8:00 pm", "Friday 4/9, 8:00 - 9:00 pm", "Friday 4/9, 8:00 - 9:00 pm", "Friday 4/9, 9:00 - 10:00 pm", "Saturday 4/10, 9:00 - 10:00 am", "Saturday 4/10, 10:00 - 11:00 am", "Saturday 4/10, 11:00 - 12:00 am", "Saturday 4/10, 12:00 - 1:00 pm", "Saturday 4/10, 1:00 - 3:00 pm", "Saturday 4/10, 3:00 - 4:00 pm", "Saturday 4/10, 4:00 - 5:00 pm", "Saturday 4/10, 5:00 - 6:00 pm", "Saturday 4/10, 6:00 - 7:00 pm", "Saturday 4/10, 7:00 - 8:00 pm", "Saturday 4/10, 7:00 - 8:00 pm", "Saturday 4/10, 8:00 - 10:00 pm", "Sunday 4/11, 7:00 - 10:00 am", "Sunday 4/11, 10:00 - 11:45 am", "Sunday 4/11, 11:45 - 12:30 am"]
+    //let descriptions = []
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 8;
+        return eventTitles.count;
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = eventsTableView.dequeueReusableCell(withIdentifier: "EventCell", for: indexPath) as! EventCell
         
         // set the question and answer labels
-        cell.title.text = "Blah"
-        cell.time.text = "Time"
+        cell.title.text = eventTitles[indexPath.row]
+        cell.time.text = times[indexPath.row]
         cell.descrip.text = "Blah blah blah"
         
         return cell
