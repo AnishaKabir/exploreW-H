5450216ab31eb8f9b72d96dd1c53f0b78527e787
Anisha Kabir <ahsina.ribak@gmail.com>
2021-04-01 18:43:50 -0700

    updated events

--- womxnhacksapp/EventsViewController.swift
+++ womxnhacksapp/EventsViewController.swift
@@ -11,10 +11,9 @@ class EventsViewController: UIViewController, UITableViewDataSource, UITableView
     
     @IBOutlet weak var eventsTableView: UITableView!
     
-    let eventTitles = ["Opening Ceremony", "Dinner and Team Formation", "Intro to iOS Workshop", "Queer and Trans 101 Workshop", "Scavenger Hunt", "Morning Meditation", "Intro to Unity", "Intro to Hardware", "Lunch", "Company Expo", "Navigating a Tech Career as a Woman Panel", "Coffee Conversations", "Intro to Deep Learning", "How to be an Ally Workshop", "Buzzfeed Quizzes", "How to Land a Job/Interview and Intro to Whatever", "Movie Night", "Devpost Submissions", "Demos for Judging", "Closing Ceremony and Prizes"]
-    let times = ["Friday 4/9, 5:00 - 6:30 pm", "Friday 4/9, 6:30 - 8:00 pm", "Friday 4/9, 8:00 - 9:00 pm", "Friday 4/9, 8:00 - 9:00 pm", "Friday 4/9, 9:00 - 10:00 pm", "Saturday 4/10, 9:00 - 10:00 am", "Saturday 4/10, 10:00 - 11:00 am", "Saturday 4/10, 11:00 - 12:00 am", "Saturday 4/10, 12:00 - 1:00 pm", "Saturday 4/10, 1:00 - 3:00 pm", "Saturday 4/10, 3:00 - 4:00 pm", "Saturday 4/10, 4:00 - 5:00 pm", "Saturday 4/10, 5:00 - 6:00 pm", "Saturday 4/10, 6:00 - 7:00 pm", "Saturday 4/10, 7:00 - 8:00 pm", "Saturday 4/10, 7:00 - 8:00 pm", "Saturday 4/10, 8:00 - 10:00 pm", "Sunday 4/11, 7:00 - 10:00 am", "Sunday 4/11, 10:00 - 11:45 am", "Sunday 4/11, 11:45 - 12:30 am"]
-    //let descriptions = []
+    let eventTitles = ["Check In", "Pre-Ceremony Game", "Opening Ceremony", "Dinner and Team Formation", "Intro to Web Dev", "Intro to Unity", "Scavenger Hunt", "Morning Meditation", "Expansion 101", "Product Management Decoded", "Git Workshop", "Company Expo", "Android and iOS Workshop", "Post Grad with CS Panel", "Coffee Conversations", "How to Land a Job/Interview", "Buzzfeed Quizzes", "Bob Ross & Security with MLH", "Devpost Submissions", "Submissions Due", "Demos for Judging", "Closing Ceremony and Prizes"]
     
+    let times = ["Friday 4/9, 11:00 am", "Friday 4/9, 4:00 - 5:00 pm", "Friday 4/9, 5:00 - 6:30 pm", "Friday 4/9, 6:30 - 8:00 pm", "Friday 4/9, 8:00 - 9:00 pm", "Friday 4/9, 9:00 - 10:00 pm", "Friday 4/9, 10:00 - 11:00 pm", "Saturday 4/10, 9:00 - 10:00 am", "Saturday 4/10, 10:00 - 11:00 am", "Saturday 4/10, 11:00 - 12:00 am", "Saturday 4/10, 12:00 - 1:00 pm", "Saturday 4/10, 1:00 - 3:00 pm", "Saturday 4/10, 3:00 - 4:00 pm", "Saturday 4/10, 4:00 - 5:00 pm", "Saturday 4/10, 5:00 - 6:00 pm", "Saturday 4/10, 6:00 - 7:00 pm", "Saturday 4/10, 7:00 - 8:00 pm", "Saturday 4/10, 8:00 - 9:00 pm", "Sunday 4/11, 7:00 - 10:00 am", "Sunday 4/11, 9:00 am", "Sunday 4/11, 10:00 - 11:45 am", "Sunday 4/11, 11:45 - 12:30 am"]    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return eventTitles.count;
@@ -32,7 +31,7 @@ class EventsViewController: UIViewController, UITableViewDataSource, UITableView
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 200;
+        return 120;
     }
     
     override func viewDidLoad() {
